55bd7560ce87b0ddf1b1f704d6ad5a5457dc1d72
Siddharth Mittal <hello@webxsid.com>
2026-05-03 12:00:28 +0530

    chore: remove Cloudflare adapter and wrangler config for static Astro site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
-import cloudflare from "@astrojs/cloudflare";
-
 export default defineConfig({
   integrations: [mdx()],
   output: "static",
   site: "https://crona.app",
-  adapter: cloudflare(),
 });
